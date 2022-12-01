26a5392649c92d49f1d401285d5d815b9a8f7496
Tyler Docherty <118209297+tyler-docherty@users.noreply.github.com>
2022-12-01 13:45:47 +0000

    Added port listen

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const { express } = require("express");
-
+const PORT = 80;
 const app = express();
 
 app.get("/", (req, res) => {
 	res.sendFile("index.html");
+});
+
+app.listen(PORT, () => {
+	console.log(PORT);
 });
