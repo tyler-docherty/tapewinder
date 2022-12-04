9186ccefedbfc5394c8f23648aa9a64e7f3596d3
tyler-docherty <tyler.jdocherty@gmail.com>
2022-12-04 16:58:22 +0000

    implemented helmet

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const path = require("path");
+const helmet = require("helmet");
 const PORT = 8080;
 const app = express();
 
 app.use("/static/", express.static("static"));
+app.use(helmet());
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			"script-src": ["'self'", "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"]
+		}
+	})
+)
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
 	const indexPath = path.resolve("./index.pug");
 	res.render(indexPath, {rootPath: __dirname}); // eslint-disable-line no-undef
-});
-
-app.get("/noPug", (req, res) => {
-	const indexPath = path.resolve("./index.html");
-	res.sendFile(indexPath);
 });
 
 app.listen(PORT, () => {
