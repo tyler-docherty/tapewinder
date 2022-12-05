6e4adfdae2c6acec8e9f9592ef9d0fa5fdfe9796
tyler-docherty <tyler.jdocherty@gmail.com>
2022-12-05 00:13:08 +0000

    added modal

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,25 @@
 const functions = require("firebase-functions");
-
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-
 const express = require("express");
 const path = require("path");
 const helmet = require("helmet");
 const app = express();
-
 app.use("/static/", express.static("static"));
 app.use(helmet());
 app.use(
 	helmet.contentSecurityPolicy({
 		directives: {
 			// allow bootstrap jsdelivr src
-			"script-src": ["'self'", "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"]
+			"script-src": ["'self'", "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"],
+			"font-src": ["*"]
 		}
 	})
-)
+);
 
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
 	const indexPath = path.resolve("./index.pug");
-	res.render(indexPath, {rootPath: __dirname});
+	res.render(indexPath, {loggedOut: true});
 });
 
 exports.app = functions.https.onRequest(app);
